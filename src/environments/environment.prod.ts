0e49b7463698bb56500a88fd542fee699e3f83b9
sam <sam@gmail.com>
2022-01-13 15:21:26 -0500

    api url changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-//   apiUrl: 'https://www.driveinqatar.com:3000/api',
-//   imageUrl: 'https://www.driveinqatar.com:3000/',
-  apiUrl: 'https://44.202.12.157:3000/api',
-  imageUrl: 'https://44.202.12.157:3000/',
+  apiUrl: 'https://www.driveinqatar.com:3000/api',
+  imageUrl: 'https://www.driveinqatar.com:3000/',
+  // apiUrl: 'https://44.202.12.157:3000/api',
+  // imageUrl: 'https://44.202.12.157:3000/',
 };
